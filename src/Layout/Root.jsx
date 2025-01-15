87172e5f9bacd2f3a7103b25f64da31c51b507ee
Md Nazir Hossain <nazirhossain.shaon@gmail.com>
2025-01-15 19:29:55 +0600

    added Footer

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Component/Navbar";
+import Footer from "../Component/Footer";
 
 const Root = () => {
   return (
     <div className="max-w-screen-xl mx-auto">
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer/>
     </div>
   );
 };
